f82d4d1a2c796e54085158608e70cb0a808cb425
Albert Stepantsev <philfromfmf@mail.ru>
2014-01-12 22:13:53 +0400

    CLI development

--- framework/Console/Application.php
+++ framework/Console/Application.php
@@ -3,14 +3,30 @@
 namespace T4\Console;
 
 
+use T4\Core\Exception;
 use T4\Core\TSingleton;
 
-class Application {
+class Application
+{
 
     use TSingleton;
 
+    const CMD_PATTERN = '~^(\/?)([^\/]*?)(\/([^\/]*?))?$~';
+    const DEFAULT_ACTION = 'default';
+
     public function run()
     {
+
+        try {
+
+            $route = $this->parseCmd($_SERVER['argv']);
+            var_dump($route);
+            die;
+
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+
         echo "Console test...\n";
         echo "argc=" . $_SERVER['argc'] . "\n";
         echo "argv=\n";
@@ -20,4 +36,23 @@ class Application {
         echo $line;
     }
 
-} 
+    protected function parseCmd($argv)
+    {
+
+        $argv = array_slice($argv, 1);
+        $cmd = array_shift($argv);
+        preg_match(self::CMD_PATTERN, $cmd, $m);
+        $commandName = $m[2];
+        $actionName = isset($m[4]) ? $m[4] : self::DEFAULT_ACTION;
+        $rootCommand = !empty($m[1]);
+
+        return [
+            'namespace' => $rootCommand ? 'T4' : 'App',
+            'command'   => $commandName,
+            'action'    => $actionName,
+            'params'    => $argv,
+        ];
+
+    }
+
+}
--- framework/t4.bat
+++ framework/t4.bat
@@ -1,1 +1,2 @@
+@echo off
 php "%~dp0t4.php" %*
