e1416736fdde9c771b1d1daeb91ffd995032ec6d
Albert Stepantsev <philfromfmf@mail.ru>
2014-02-03 17:10:19 +0400

    MVC development - modules

---
+++ framework/Cabs/Webapp/protected/Modules/Admin/Controllers/Index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Modules\Admin\Controllers;
+
+use T4\Mvc\Controller;
+
+class Index
+    extends Controller
+{
+
+    public function actionDefault()
+    {
+        echo 'TEST';
+        die;
+    }
+
+} 
