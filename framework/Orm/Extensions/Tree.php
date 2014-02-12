16a0b59c8f7c90e466031e7c6bfa8c79d7b1398b
Albert Stepantsev <philfromfmf@mail.ru>
2014-02-12 18:58:03 +0400

    ORM extensions development - Tree extension

--- framework/Orm/Extensions/Tree.php
+++ framework/Orm/Extensions/Tree.php
@@ -2,6 +2,7 @@
 
 namespace T4\Orm\Extensions;
 
+use T4\Dbal\QueryBuilder;
 use T4\Orm\Exception;
 use T4\Orm\Extension;
 
@@ -28,6 +29,44 @@ class Tree
             '__key' => ['columns' => ['__lft', '__rgt', '__lvl']],
             '__prt' => ['columns' => ['__prt']],
         ];
+    }
+
+    public function beforeSave($model)
+    {
+        $class = get_class($model);
+        $tableName = $class::getTableName();
+        /** @var $connection \T4\Dbal\Connection */
+        $connection = $class::getDbConnection();
+
+        if (empty($model->__prt)) {
+            $model->__prt = 0;
+            $query = new QueryBuilder();
+            $query->select('MAX(__rgt)')->from($tableName);
+            $right = $connection->query($query->getQuery())->fetchScalar() + 1;
+            $level = 0;
+        } else {
+            $parent = $class::findByPk($model->__prt);
+            if (empty($parent))
+                return false;
+            $right = $parent->__rgt;
+            $level = $parent->__lvl;
+        }
+
+        $result = $connection->execute("
+            UPDATE `" . $tableName . "`
+            SET
+                `__rgt`=:right+2,
+                `__lft`=IF(`__lft`>:right, `__lft` + 2, `__lft`) WHERE `__rgt`>=:right
+        ", [':right'=>$right]);
+        if (!$result) {
+            return false;
+        } else {
+            $model->__lft = $right;
+            $model->__rgt = $right+1;
+            $model->__lvl = $level+1;
+            return true;
+        }
+
     }
 
     public function callStatic($class, $method, $argv)
