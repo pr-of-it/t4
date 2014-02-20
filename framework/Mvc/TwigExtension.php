79dee14c3dd497c3a21187ee473035e795aa862b
Albert Stepantsev <philfromfmf@mail.ru>
2014-02-20 17:53:59 +0400

    MVC development - assets manager

--- framework/Mvc/TwigExtension.php
+++ framework/Mvc/TwigExtension.php
@@ -16,6 +16,7 @@ class TwigExtension extends \Twig_Extension
         $app = Application::getInstance();
         return [
             'asset' => new \Twig_Function_Function($app->assets),
+            'publish' => new \Twig_Function_Function(function ($path) use ($app) {$app->assets->publish($path);return '';}),
             'publishCss' => new \Twig_Function_Function(function () use ($app) { return $app->assets->getPublishedCss();},  ['is_safe' => ['html']]),
             'publishJs' => new \Twig_Function_Function(function () use ($app) { return $app->assets->getPublishedJs();},  ['is_safe' => ['html']]),
         ];
