ebd85e846aa2014d34c41a0c24b1a50cb38f0050
Albert Stepantsev <philfromfmf@mail.ru>
2014-02-27 13:40:51 +0400

    HTML helpers development

--- framework/Html/Helpers.php
+++ framework/Html/Helpers.php
@@ -7,38 +7,61 @@ use T4\Orm\Model;
 class Helpers
 {
 
-    const TREE_LEVEL_SYMBOL = '--';
+    const TREE_LEVEL_SYMBOL = '-';
 
-    static public function select($data, $value = Model::PK, $title = 'title', $selected = 0, $htmlOptions = [])
+    static public function blockOptionInput($name, $options, $value = null, $htmlOptions=[])
     {
-        $html = '<select>' . "\n";
+        switch ($options['type']) {
+            case 'select:tree':
+                $htmlOptions['name'] = $name;
+                $options['model'] = '\\App\\Models\\'.$options['model'];
+                return self::selectTreeByModel($options['model'], is_null($value) ? $options['default'] : $value, $htmlOptions);
+        }
+    }
+
+    static public function select($data, $selected = 0, $htmlOptions = [], $options=[])
+    {
+        if (empty($options['valueColumn']))
+            $options['valueColumn'] = Model::PK;
+        if (empty($options['titleColumn']))
+            $options['titleColumn'] = 'title';
+        if (is_array($selected))
+            $selected = $selected[$options['valueColumn']];
+        if ($selected instanceof Model) {
+            $class = get_class($selected);
+            $selected = $selected->{$class::PK};
+        }
+
+        $html = '<select' .
+            (isset($htmlOptions['name']) ? ' name="' . $htmlOptions['name'] . '"': '') .
+            (isset($htmlOptions['id']) ? ' id="' . $htmlOptions['id'] . '"': '') .
+            (in_array('disabled', $htmlOptions) ? ' disabled="disabled"': '') .
+            '>' . "\n";
         foreach ($data as $item) {
             $html .=
-                '<option value="' . $item[$value] . '"' . ($item[$value] == $selected ? ' selected="selected"' : '') . '>' .
-                $item[$title] .
+                '<option
+                    value="' . $item[$options['valueColumn']] . '"' .
+                    ($item[$options['valueColumn']] == $selected ? ' selected="selected"' : '') .
+                '>' .
+                ( in_array('tree', $options) ? str_repeat(self::TREE_LEVEL_SYMBOL, (int)$item[$options['treeLevelColumn']]) : '' ) . ' ' . $item[$options['titleColumn']] .
                 '</option>' . "\n";
         }
         $html .= '</select>';
         return $html;
     }
 
-    static public function selectTree($data, $value = Model::PK, $title = 'title', $selected = 0, $htmlOptions = [])
+    static public function selectTree($data, $selected = 0, $htmlOptions = [], $options=[])
     {
-        $html = '<select>' . "\n";
-        foreach ($data as $item) {
-            $html .=
-                '<option value="' . $item[$value] . '"' . ($item[$value] == $selected ? ' selected="selected"' : '') . '>' .
-                str_repeat(self::TREE_LEVEL_SYMBOL, $item['level']) . $item[$title] .
-                '</option>' . "\n";
-        }
-        $html .= '</select>';
-        return $html;
+        $options = array_merge($options, ['tree']);
+        if (empty($options['treeLevelColumn']))
+            $options['treeLevelColumn'] = '__lvl';
+        return self::select($data, $selected, $htmlOptions, $options);
     }
 
-    static public function selectTreeByModel($model, $title = 'title', $selected = 0, $htmlOptions = [])
+    static public function selectTreeByModel($model, $selected = 0, $htmlOptions = [], $options=[])
     {
         $data = $model::findAllTree();
-        return self::selectTree($data, $model::PK, $title, $selected, $htmlOptions);
+        return self::selectTree($data, $selected, $htmlOptions, $options);
     }
 
 }
--- framework/Mvc/TwigExtension.php
+++ framework/Mvc/TwigExtension.php
@@ -2,6 +2,7 @@
 
 namespace T4\Mvc;
 
+use T4\Html\Helpers;
 
 class TwigExtension extends \Twig_Extension
 {
@@ -15,10 +16,18 @@ class TwigExtension extends \Twig_Extension
     {
         $app = Application::getInstance();
         return [
+
             'asset' => new \Twig_Function_Function($app->assets),
             'publish' => new \Twig_Function_Function(function ($path) use ($app) {$app->assets->publish($path);return '';}),
             'publishCss' => new \Twig_Function_Function(function () use ($app) { return $app->assets->getPublishedCss();},  ['is_safe' => ['html']]),
             'publishJs' => new \Twig_Function_Function(function () use ($app) { return $app->assets->getPublishedJs();},  ['is_safe' => ['html']]),
+
+            'blockOptionInput' => new \Twig_Function_Function(
+                    function ($name, $settings, $value=null, $htmlOptions=[]) use ($app) {
+                        return Helpers::blockOptionInput($name, $settings, $value, $htmlOptions);
+                    },  ['is_safe' => ['html']]
+            ),
+
         ];
     }
 
