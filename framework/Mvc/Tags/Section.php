f293797d7ba954d0814678140a6b83268a8e783a
Albert Stepantsev <philfromfmf@mail.ru>
2014-03-05 19:02:03 +0400

    MVC development - tag 'section' development

--- framework/Mvc/Tags/Section.php
+++ framework/Mvc/Tags/Section.php
@@ -11,7 +11,16 @@ class Section
     protected function render()
     {
         $id = $this->params->id;
-        return 'Section #'.$id;
+        $app = Application::getInstance();
+        $blocks = App\Models\Block::findAllBySection($id);
+
+        $ret = '<section role="section" data-section-id="' . $id . '">';
+        foreach ($blocks as $block) {
+            $ret .= '<article role="block" data-block-id="' . $block->getPk() . '">' .
+                $app->callBlock($block->path, json_decode($block->options, true)) .
+                '</article>';
+        }
+        return $ret . '</section>';
     }
 
 }
