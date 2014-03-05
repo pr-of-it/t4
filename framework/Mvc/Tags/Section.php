5e2f007ca4ceb3c7e15b47cccbaf6e5228c09d78
Albert Stepantsev <philfromfmf@mail.ru>
2014-03-05 19:16:31 +0400

    MVC development - tag 'section' development

--- framework/Mvc/Tags/Section.php
+++ framework/Mvc/Tags/Section.php
@@ -16,7 +16,7 @@ class Section
         $app = Application::getInstance();
         $blocks = \App\Models\Block::findAllBySection($id);
 
-        $ret = '<section role="section" data-section-id="' . $id . '">';
+        $ret = '<section role="section" data-section-id="' . $id . '">' . "\n";
         foreach ($blocks as $block) {
             $ret .= '<article role="block" data-block-id="' . $block->getPk() . '">' .
                 $app->callBlock($block->path, new Std(json_decode($block->options, true))) .
