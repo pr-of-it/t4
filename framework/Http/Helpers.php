f60ccb3076d56d2b943c5735603c4d4e2517c4d3
Albert Stepantsev <philfromfmf@mail.ru>
2014-03-26 16:03:45 +0400

    redirect method fix

--- framework/Http/Helpers.php
+++ framework/Http/Helpers.php
@@ -7,7 +7,7 @@ class Helpers
 
     public static function redirect($url)
     {
-        header('Location: ' . $url, true, 303);
+        header('Location: ' . (empty($_SERVER['HTTPS']) || 'off'==$_SERVER['HTTPS'] ? 'http://' : 'https://' ).$_SERVER['HTTP_HOST'].$url, true, 302);
         exit;
     }
 
