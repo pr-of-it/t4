277f87d38555240be8c5e936737525953a95cceb
phil <philfromfmf@mail.ru>
2014-06-22 16:05:16 +0400

    ! Uploader development

--- framework/Http/Uploader.php
+++ framework/Http/Uploader.php
@@ -3,6 +3,7 @@
 namespace T4\Http;
 
 use T4\Core\Exception;
+use T4\Fs\Helpers;
 
 class Uploader
 {
@@ -10,7 +11,7 @@ class Uploader
     protected $formFieldName = '';
     protected $uploadPath;
 
-    public function __construct($name='')
+    public function __construct($name = '')
     {
         $this->formFieldName = $name;
     }
@@ -20,9 +21,19 @@ class Uploader
         $this->uploadPath = $path;
     }
 
-    public function __invoke($name='')
+    public function __invoke($name = '')
     {
+        if (empty($this->uploadPath))
+            throw new Exception('Invalid upload path');
+
         $realUploadPath = ROOT_PATH_PUBLIC . str_replace('/', DS, $this->uploadPath);
+        if (!is_dir($realUploadPath)) {
+            try {
+                Helpers::mkDir($realUploadPath);
+            } catch (\T4\Fs\Exception $e) {
+                throw new Exception($e->getMessage());
+            }
+        }
 
         if (empty($this->formFieldName) && !empty($name))
             $this->formFieldName = $name;
@@ -30,19 +41,19 @@ class Uploader
         if (empty($this->formFieldName))
             throw new Exception('Empty form field name for file upload');
 
-        if (empty($this->uploadPath) && !is_readable($realUploadPath) && !is_dir($realUploadPath))
-            throw new Exception('Invalid upload path');
-
-        if (empty($_FILES[$this->formFieldName]) || 0==$_FILES[$this->formFieldName]['size'])
+        if (empty($_FILES[$this->formFieldName]) || 0 == $_FILES[$this->formFieldName]['size'])
             throw new Exception('File \'' . $this->formFieldName . '\' is not uploaded');
 
         if (\UPLOAD_ERR_OK != $_FILES[$this->formFieldName]['error'])
-            throw new Exception('Upload error while uploading file \'' . $this->formFieldName . '\': '.$_FILES[$this->formFieldName]['error']);
+            throw new Exception('Upload error while uploading file \'' . $this->formFieldName . '\': ' . $_FILES[$this->formFieldName]['error']);
 
-        $file = $realUploadPath . DS . basename($_FILES[$this->formFieldName]['name']);
-        $ret = str_replace(DS, '/', $this->uploadPath) . '/' . basename($_FILES[$this->formFieldName]['name']);
+        $uploadedFileName = basename($_FILES[$this->formFieldName]['name']);
+        while ( file_exists($realUploadPath . DS . $uploadedFileName) ) {
+            $uploadedFileName = pathinfo($uploadedFileName, PATHINFO_FILENAME) . '_.' . pathinfo($uploadedFileName, PATHINFO_EXTENSION);
+        };
+        $ret = str_replace(DS, '/', $this->uploadPath) . '/' . $uploadedFileName;
 
-        if (!move_uploaded_file($_FILES[$this->formFieldName]['tmp_name'], $file)) {
+        if (!move_uploaded_file($_FILES[$this->formFieldName]['tmp_name'], $realUploadPath . DS . $uploadedFileName)) {
             throw new Exception('Save uploaded file error');
         }
 
@@ -50,4 +61,4 @@ class Uploader
 
     }
 
-} 
+}
