e95f38a48c9d2f119e8bb838d5bad9789853bc7b
Albert Stepantsev <philfromfmf@mail.ru>
2014-06-23 15:51:32 +0400

    ! some refactoring in blocks

--- framework/Cabs/Webapp/protected/Models/Block.php
+++ framework/Cabs/Webapp/protected/Models/Block.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use T4\Mvc\Application;
+use T4\Mvc\Router;
 use T4\Orm\Model;
 
 class Block
@@ -17,4 +19,18 @@ class Block
             'order'     => ['type'=>'int', 'default'=>0],
         ],
     ];
+
+    public function getAllTemplates()
+    {
+        $route = Router::getInstance()->splitInternalPath($this->path);
+        $controller = Application::getInstance()->createController($route->module, $route->controller);
+        $templates = [];
+        foreach ($controller->getTemplatePaths() as $path) {
+            foreach (glob($path . DS . $route->action . '.*.block.html') as $filename) {
+                preg_match('~.*\.([^\.]+)\.block\.html~', basename($filename), $m);
+                $templates[] = $m[1];
+            }
+        }
+        return $templates;
+    }
 }
