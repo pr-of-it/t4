d4e900abd5a2f5daf2a6838b7ec804c5f266217b
Степанцев Альберт Сергеевич <astepantsev@corp.finam.ru>
2014-12-15 17:53:03 +0300

    ! threads development

---
+++ tests/Threads/HelpersTest.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once realpath(__DIR__ . '/../../framework/boot.php');
+
+class HelpersTest extends PHPUnit_Framework_TestCase {
+
+    public $testFileName;
+
+    public function setUp()
+    {
+        $this->testFileName = __DIR__ . '/test.txt';
+    }
+
+    public function testAsyncRun()
+    {
+        $testFileName = $this->testFileName;
+        $this->assertFalse(file_exists($testFileName));
+
+        \T4\Threads\Helpers::run(function ($text) use ($testFileName) {
+            sleep(10);
+            file_put_contents($testFileName, $text);
+        }, ['text'=>'Hello, world!']);
+
+        sleep(5);
+        $this->assertFalse(file_exists($testFileName));
+        sleep(7);
+        $this->assertTrue(file_exists($testFileName));
+        $this->assertEquals(
+            'Hello, world!',
+            file_get_contents($testFileName)
+        );
+    }
+
+    public function tearDown()
+    {
+        unlink($this->testFileName);
+    }
+
+}
+ 
