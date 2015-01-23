d796f16b573e9c744a421ccf5a04bef9bb987994
Степанцев Альберт Сергеевич <astepantsev@corp.finam.ru>
2015-01-23 12:49:38 +0300

    + SxGeo extension is added

--- framework/Http/Request.php
+++ framework/Http/Request.php
@@ -138,22 +138,6 @@ class Request
         return $ret;
     }
 
-    public function getLocation()
-    {
-
-        if($location=Session::get(location_cache)){
-
-        } else {
-
-            $locator = new Locator();
-            $location= $locator->getLocation();
-            Session::set(location_cache, $location);
-            $location= Session::get(location_cache);
-        }
-
-        return $location;
-    }
-
     public function getIp()
     {
         if(isset($_SERVER['REMOTE_ADDR'])) {
