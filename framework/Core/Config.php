43090c117c2e5ea1fbf02f1d61772d1836284834
gembux2012@gmail.com <gembux2012@gmail.com>
2015-03-11 19:55:09 +0600

    Merge branch 'configsave' of https://github.com/pr-of-it/t4 into configsave
    Conflicts:
    	framework/Core/Config.php
    	tests/Core/config.test.php

--- framework/Core/Config.php
+++ framework/Core/Config.php
@@ -36,8 +36,11 @@ class Config extends Std
     public function save($path)
     {
      //   $path=ROOT_PATH_PROTECTED . DS . 'config.php';
+        $str=var_export($this->toArray());
+        $str=str_replace( [ '(', ')' ], [ '[', ']' ], $str );
         $file = fopen($path, 'w');
-        fwrite($file,var_export($this->toArray(),true));
+        //fwrite($file,var_export($this->toArray(),true));
+        fwrite($file,str,true);
 
         fclose($file);
 
