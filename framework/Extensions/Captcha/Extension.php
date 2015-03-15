de827758607e71c28764d20476782b479f4e5717
Albert Stepantsev <philfromfmf@mail.ru>
2015-03-15 16:31:40 +0300

    ! Captcha extension development

--- framework/Extensions/Captcha/Extension.php
+++ framework/Extensions/Captcha/Extension.php
@@ -8,21 +8,23 @@ class Extension
     extends \T4\Core\Extension
 {
 
+    const KEYSTRING_KEY = 'captcha_keystring';
+
     public function init()
     {
         require_once __DIR__ . DS . 'src' . DS . 'kcaptcha.php';
     }
 
-    public function generateImage()
+    public function generateImage($config = null)
     {
         $captcha = new \KCAPTCHA();
-        Session::set('keystring', $captcha->getKeyString());
+        Session::set(self::KEYSTRING_KEY, $captcha->getKeyString());
         die;
     }
 
     public function checkKeyString($string)
     {
-        return Session::get('keystring') == $string;
+        return Session::get(self::KEYSTRING_KEY) == $string;
     }
 
 }
