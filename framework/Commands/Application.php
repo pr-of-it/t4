0807fbede7224e0dd883fb9acf5e5446eeafd9e2
Степанцев Альберт <126@gosbroker.ru>
2015-03-20 17:11:58 +0300

    ! Standard app is removed from framework

--- framework/Commands/Application.php
+++ framework/Commands/Application.php
@@ -11,15 +11,8 @@ class Application
     public function actionDefault()
     {
         $this->writeLn('T4 Console. Use it:');
-        $this->writeLn('- t4 /create/app for T4 web-application quick-start');
         $this->writeLn('- t4 /command/action for pre-installed T4 commands');
         $this->writeLn('- t4 command/action for your application commands');
     }
 
-    public function actionCreate()
-    {
-        $_SERVER['argv'][1] = '/create/app';
-        $this->app->run();
-    }
-
-} 
+}
--- framework/Commands/Create.php
+++ framework/Commands/Create.php
@@ -10,59 +10,6 @@ class Create
     extends Command
 {
 
-    public function actionApp()
-    {
-        $src = \T4\ROOT_PATH.DS.'Cabs'.DS.'Webapp';
-        $dst = ROOT_PATH;
-        echo 'Web-application install'."\n";
-
-        $dst = $this->read('Root path for your application', $dst);
-
-        $publicDirName = 'www';
-        $publicDirName = $this->read('Public path for your application', $publicDirName);
-
-        // TODO: починить эту мерзость в классе Std
-        $settings = new Std();$settings->db = new Std();$settings->db->default = new Std();
-
-        $settings->db->default->driver = 'mysql';
-        $settings->db->default->driver = $this->read('Default DB driver', $settings->db->default->driver);
-
-        $settings->db->default->host = 'localhost';
-        $settings->db->default->host = $this->read('Default DB hostname', $settings->db->default->host);
-
-        $settings->db->default->dbname = 't4';
-        $settings->db->default->dbname = $this->read('Default DB database name', $settings->db->default->dbname);
-
-        $settings->db->default->user = $this->read('Default DB user name');
-
-        $settings->db->default->password = $this->read('Default DB password', '', false);
-
-        \T4\Fs\Helpers::copyDir($src, $dst);
-        $this->writeLn('Application files are copied');
-
-        $configFileName = $dst.DS.'protected'.DS.'config.php';
-        $configFile = file_get_contents($configFileName);
-        foreach ($settings->db->default as $name=>$value)
-            $configFile = str_replace('{{'.$name.'}}', $value, $configFile);
-        file_put_contents($configFileName, $configFile);
-
-        rename($dst.DS.'www', $dst.DS.$publicDirName);
-
-        $command = new Migrate();
-        $command->action('up');
-        $this->writeLn('Systems tables are created');
-
-        $user = new User();
-        $user->email = $this->read('Super user email');
-        $user->password = $this->read('Super user password', '', false);
-        $user->password = \T4\Crypt\Helpers::hashPassword($user->password);
-        $user->save();
-        $this->writeLn('Super user is created');
-
-        $this->writeLn('Application is installed!');
-
-    }
-
     public function actionExtension($name) {
         $name = ucfirst($name);
         $dirname = ROOT_PATH_PROTECTED.DS.'Extensions'.DS.$name;
