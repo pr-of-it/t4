736645a16399a76e3c6cfd98d6fb88569c5fb33d
Степанцев Альберт <126@gosbroker.ru>
2015-04-10 14:10:56 +0400

    + Form\Errors is added

--- framework/Html/Form/Errors.php
+++ framework/Html/Form/Errors.php
@@ -12,9 +12,20 @@ class Errors
 
     public function add($field, $error = "", $code = 0, $severity = 1, $filename = __FILE__, $lineno = __LINE__, $previous = null)
     {
-        $exception = new Exception($error, $code, $severity, $filename, $lineno, $previous);
-        $this->errors[$field][] = $exception;
-        parent::add($exception);
+        if ($error instanceof Exception) {
+            if ($error instanceof $this->class) {
+                $this[] = $error;
+            } else {
+                throw new Exception('Incompatible exception class' . get_class($error));
+            }
+        } else {
+            $class = $this->class;
+            $error = new $class($error, $code, $severity, $filename, $lineno, $previous);
+            $this[] = $error;
+        }
+
+        $this->errors[$field][] = $error;
+        return $this;
     }
 
     public function getErrorsForField($field)
