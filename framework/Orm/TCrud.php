aa0249a40b14d45c378cb0bf58875f9cb6ee6ec6
Степанцев Альберт <126@gosbroker.ru>
2015-06-05 11:05:59 +0400

    + PHPDoc annotations

--- framework/Orm/TCrud.php
+++ framework/Orm/TCrud.php
@@ -9,33 +9,53 @@ trait TCrud
     protected $wasNew = false;
     protected $isDeleted = false;
 
+    /**
+     * @param bool $new
+     * @return $this
+     */
     public function setNew($new)
     {
         $this->isNew = $new;
         return $this;
     }
 
+    /**
+     * @return bool
+     */
     public function isNew()
     {
         return $this->isNew;
     }
 
+    /**
+     * @return bool
+     */
     public function wasNew()
     {
         return $this->wasNew;
     }
 
+    /**
+     * @param bool $deleted
+     * @return $this
+     */
     public function setDeleted($deleted)
     {
         $this->isDeleted = $deleted;
         return $this;
     }
 
+    /**
+     * @return bool
+     */
     public function isDeleted()
     {
         return $this->isDeleted;
     }
 
+    /**
+     * @return $this
+     */
     public function refresh()
     {
         if ($this->isNew())
@@ -52,7 +72,7 @@ trait TCrud
     /**
      * @param string|\T4\Dbal\QueryBuilder $query
      * @param array $params
-     * @return \T4\Orm\Model
+     * @return static
      */
     public static function findAllByQuery($query, $params = [])
     {
@@ -65,7 +85,7 @@ trait TCrud
     /**
      * @param string|\T4\Dbal\QueryBuilder $query
      * @param array $params
-     * @return \T4\Orm\Model
+     * @return static
      */
     public static function findByQuery($query, $params = [])
     {
@@ -79,7 +99,7 @@ trait TCrud
 
     /**
      * @param array $options
-     * @return \T4\Core\Collection
+     * @return \T4\Core\Collection|static[]
      */
     public static function findAll($options = [])
     {
@@ -91,7 +111,7 @@ trait TCrud
 
     /**
      * @param array $options
-     * @return \T4\Orm\Model
+     * @return static
      */
     public static function find($options = [])
     {
@@ -107,7 +127,7 @@ trait TCrud
      * @param string $column
      * @param mixed $value
      * @param array $options
-     * @return \T4\Core\Collection
+     * @return \T4\Core\Collection|static[]
      */
     public static function findAllByColumn($column, $value, $options = [])
     {
@@ -121,7 +141,7 @@ trait TCrud
      * @param string $column
      * @param mixed $value
      * @param array $options
-     * @return \T4\Orm\Model
+     * @return static
      */
     public static function findByColumn($column, $value, $options = [])
     {
@@ -135,19 +155,29 @@ trait TCrud
 
     /**
      * @param mixed $value
-     * @return \T4\Orm\Model
+     * @return static
      */
     public static function findByPK($value)
     {
         return static::findByColumn(static::PK, $value);
     }
 
+    /**
+     * @param array $options
+     * @return int
+     */
     public static function countAll($options = [])
     {
         $driver = static::getDbDriver();
         return $driver->countAll(get_called_class(), $options);
     }
 
+    /**
+     * @param string $column
+     * @param mixed $value
+     * @param array $options
+     * @return int
+     */
     public static function countAllByColumn($column, $value, $options = [])
     {
         $driver = static::getDbDriver();
