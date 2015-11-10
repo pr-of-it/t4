0bc24b35779361e86e77536abaff0458088ca861
Альберт Степанцев <126@gosbroker.ru>
2015-11-10 12:41:33 +0300

    + Collection development

---
+++ framework/Core/ICollection.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace T4\Core;
+
+interface ICollection
+{
+
+    public function add($value);
+    public function prepend($value);
+    public function append($value);
+
+    public function existsElement(array $attributes);
+    public function findAllByAttributes(array $attributes);
+    public function findByAttributes(array $attributes);
+
+    public function asort();
+    public function ksort();
+    public function uasort(callable $callback);
+    public function uksort(callable $callback);
+    public function natsort();
+    public function natcasesort();
+    public function sort(callable $callback);
+
+    public function map(callable $callback);
+    public function filter(callable $callback);
+    public function reduce($start, callable $callback);
+    public function collect($what);
+    public function group($by);
+
+    public function __call($method, array $params = []);
+
+}
---
+++ framework/Core/TCollection.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace T4\Core;
+
+trait TCollection
+{
+    use TArrayAccess;
+
+    /**
+     * @param $value
+     * @return $this
+     */
+    public function add($value)
+    {
+        return $this->append($value);
+    }
+
+    /**
+     * @param $value
+     * @return $this
+     */
+    public function prepend($value)
+    {
+        $this->storage = array_merge([$value], $this->storage);
+        return $this;
+    }
+
+    /**
+     * @param $value
+     * @return $this
+     */
+    public function append($value)
+    {
+        $this->storage = array_merge($this->storage, [$value]);
+        return $this;
+    }
+
+    public function existsElement(array $attributes)
+    {
+        if (empty($attributes))
+            return false;
+        foreach ($this as $element) {
+            $elementAttributes = [];
+            if (!is_array($element) && !(is_object($element) && $element instanceof \Traversable)) {
+                continue;
+            }
+            foreach ($element as $key => $val) {
+                if (array_key_exists($key, $attributes))
+                    $elementAttributes[$key] = $val;
+            }
+            if ($attributes == $elementAttributes)
+                return true;
+        }
+        return false;
+    }
+
+    public function findAllByAttributes(array $attributes)
+    {
+        return $this->filter(function ($x) use ($attributes) {
+            $elementAttributes = [];
+            foreach ($x as $key => $value) {
+                if (array_key_exists($key, $attributes)) {
+                    $elementAttributes[$key] = $value;
+                }
+            }
+            return $elementAttributes == $attributes;
+        });
+    }
+
+    public function findByAttributes(array $attributes)
+    {
+        $allCollection = $this->findAllByAttributes($attributes);
+        return $allCollection->isEmpty() ? null : $allCollection[0];
+    }
+
+    /**
+     * @return static
+     */
+    public function asort()
+    {
+        $copy = $this->toArray();
+        asort($copy);
+        return new static($copy);
+    }
+
+    /**
+     * @return static
+     */
+    public function ksort()
+    {
+        $copy = $this->toArray();
+        ksort($copy);
+        return new static($copy);
+    }
+
+    /**
+     * @param callable $callback
+     * @return static
+     */
+    public function uasort(callable $callback) {
+        $copy = $this->toArray();
+        uasort($copy, $callback);
+        return new static($copy);
+    }
+
+    /**
+     * @param callable $callback
+     * @return static
+     */
+    public function uksort(callable $callback) {
+        $copy = $this->toArray();
+        uksort($copy, $callback);
+        return new static($copy);
+    }
+
+    /**
+     * @return static
+     */
+    public function natsort() {
+        $copy = $this->toArray();
+        natsort($copy);
+        return new static($copy);
+    }
+
+    /**
+     * @return static
+     */
+    public function natcasesort() {
+        $copy = $this->toArray();
+        natcasesort($copy);
+        return new static($copy);
+    }
+
+    /**
+     * @param callable $callback
+     * @return static
+     */
+    public function sort(callable $callback)
+    {
+        return $this->uasort($callback);
+    }
+
+    /**
+     * @param callable $callback
+     * @return static
+     */
+    public function map(callable $callback)
+    {
+        return new static(array_values(array_map($callback, $this->toArray())));
+    }
+
+    /**
+     * @param callable $callback
+     * @return static
+     */
+    public function filter(callable $callback)
+    {
+        return new static(array_values(array_filter($this->toArray(), $callback)));
+    }
+
+    /**
+     * @param mixed $start
+     * @param callable $callback
+     * @return static
+     */
+    public function reduce($start, callable $callback)
+    {
+        return new static(array_values(array_reduce($this->toArray(), $callback, $start)));
+    }
+
+    /**
+     * @param mixed $what
+     * @return array
+     */
+    public function collect($what)
+    {
+        $ret = [];
+        foreach ($this as $element) {
+            if ($what instanceof \Closure) {
+                $ret[] = $what($element);
+            } elseif (is_array($element)) {
+                $ret[] = $element[$what];
+            } elseif (is_object($element)) {
+                $ret[] = $element->$what;
+            }
+        }
+        return $ret;
+    }
+
+    /**
+     * @param $by
+     * @return array|static[]
+     */
+    public function group($by)
+    {
+        $ret = [];
+        foreach ($this as $element) {
+            if ($by instanceof \Closure) {
+                $key = $by($element);
+            } elseif (is_array($element)) {
+                $key = $element[$by];
+            } elseif (is_object($element)) {
+                $key = $element->$by;
+            }
+            if (!isset($ret[$key])) {
+                $ret[$key] = new static;
+            }
+            $ret[$key]->add($element);
+        }
+        return $ret;
+    }
+
+    public function __call($method, array $params = [])
+    {
+        foreach ($this as $element) {
+            call_user_func_array([$element, $method], $params);
+        }
+    }
+
+}
