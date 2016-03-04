42e62485e6f98cca466e3eb9027f3829b786cccf
Альберт Степанцев <126@gosbroker.ru>
2016-03-04 11:35:45 +0300

    ! Ckfinder fix

--- framework/Extensions/Ckfinder/Extension.php
+++ framework/Extensions/Ckfinder/Extension.php
@@ -12,6 +12,9 @@ class Extension
         $assets = $this->app->assets;
         $assetsUrl = $assets->publish($this->assetsPath.'/lib');
 
+        $fileName = ROOT_PATH_PUBLIC . $assetsUrl . '/config.php';
+        file_put_contents($fileName, str_replace(['{{ROOT_PATH_PROTECTED}}', '{{ROOT_PATH_T4}}'], [ROOT_PATH_PROTECTED, \T4\ROOT_PATH], file_get_contents($fileName)));
+
         $assets->publishJsFile($this->assetsPath.'/lib/ckfinder.js');
 
         $assets->registerJs(<<<FILE
