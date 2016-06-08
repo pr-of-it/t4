7ee2ba9d386e6b7c76166a4bef0ef79758e8c30c
Альберт Степанцев <126@gosbroker.ru>
2016-06-08 15:19:20 +0300

    + Condition class is added

---
+++ framework/Dbal/Condition.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace T4\Dbal;
+
+use T4\Core\Std;
+
+class Condition
+{
+
+    const UNARY_OPERATORS = ['NOT'];
+    const BINARY_OPERATORS = ['=', '<>', '!=', '<', '>', '<=', '>=', 'IN', 'LIKE'];
+
+    protected $x;
+    protected $operator;
+    protected $y;
+
+    protected $params = [];
+
+    public function __construct($data)
+    {
+        if (is_array($data) || $data instanceof \ArrayAccess) {
+            $this->x = $data['x'] ?? null;
+            $this->operator = $data['operator'] ?? null;
+            $this->y = $data['y'] ?? null;
+            $this->params = $data['params'] ?? [];
+        } else {
+            $this->fromString((string)$data);
+        }
+    }
+
+    public function fromString($str)
+    {
+        foreach (self::UNARY_OPERATORS as $operator) {
+            if (preg_match('~^' . preg_quote($operator, '~') . '\s*(?P<x>\S+)$~i', trim($str), $m)) {
+                $this->operator = $operator;
+                $this->x = $m['x'];
+                return $this;
+            }
+        }
+        foreach (self::BINARY_OPERATORS as $operator) {
+            if ( preg_match('~^(?P<x>\S+)\s*' . preg_quote($operator, '~') . '\s*(?P<y>\S+)$~i', trim($str), $m) ) {
+                $this->x = $m['x'];
+                $this->operator = $operator;
+                $this->y = $m['y'];
+                return $this;
+            }
+        }
+    }
+
+    public function toString()
+    {
+        switch ( true ) {
+            case in_array($this->operator, self::UNARY_OPERATORS):
+                return $this->operator . ' ' . $this->x;
+                break;
+            case in_array($this->operator, self::BINARY_OPERATORS):
+                return $this->x . ' ' . $this->operator . ' ' . $this->y;
+                break;
+        }
+    }
+
+    public function __toString()
+    {
+        return $this->toString();
+    }
+    
+    public function setParams(array $params = [])
+    {
+        $this->params = $params;
+        return $this;
+    }
+
+}
---
+++ tests/Dbal/ConditionTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace T4\Tests\Dbal {
+
+    use T4\Dbal\Condition;
+
+    require_once realpath(__DIR__ . '/../../framework/boot.php');
+
+    class ConditionTest
+        extends \PHPUnit_Framework_TestCase
+    {
+        
+        public function testCreateUnary()
+        {
+            $condition = new Condition('NOT 42');
+            
+            $reflector = new \ReflectionObject($condition);
+
+            $x = $reflector->getProperty('x');
+            $x->setAccessible(true);
+            $this->assertEquals('42', $x->getValue($condition));
+
+            $operator = $reflector->getProperty('operator');
+            $operator->setAccessible(true);
+            $this->assertEquals('NOT', $operator->getValue($condition));
+        }
+
+        public function testCreateBinary()
+        {
+            $condition = new Condition('12 < 13');
+
+            $reflector = new \ReflectionObject($condition);
+
+            $x = $reflector->getProperty('x');
+            $x->setAccessible(true);
+            $this->assertEquals('12', $x->getValue($condition));
+
+            $operator = $reflector->getProperty('operator');
+            $operator->setAccessible(true);
+            $this->assertEquals('<', $operator->getValue($condition));
+
+            $y = $reflector->getProperty('y');
+            $y->setAccessible(true);
+            $this->assertEquals('13', $y->getValue($condition));
+        }
+        
+        public function testToStringUnary()
+        {
+            $condition = new Condition(' NOT   42 ');
+            $this->assertEquals('NOT 42', $condition->toString());
+
+            $condition = new Condition(['operator' => 'NOT', 'x' => 13]);
+            $this->assertEquals('NOT 13', $condition->__toString());
+        }
+
+        public function testToStringBinary()
+        {
+            $condition = new Condition(' 13 <   42 ');
+            $this->assertEquals('13 < 42', $condition->toString());
+
+            $condition = new Condition(['x' => 42, 'operator' => '>', 'y' => 13]);
+            $this->assertEquals('42 > 13', $condition->__toString());
+        }
+
+    }
+
+}
