e28fd86bc3490aacde049ae2c36136dc215db825
coooller@gmail.com <Ghjdjlbnm1>
2016-06-12 16:17:20 +0300

    Класс QueryString

---
+++ framework/Core/QueryString.php
@@ -0,0 +1,32 @@
+<?php
+namespace T4\Core;
+
+class QueryString
+  extends Collection
+{
+    public function __construct($data = null)
+    {
+        if (null !== $data && is_string($data)) {
+            $this->fromString($data);
+        } else {
+            parent::__construct($data);
+        }
+    }
+
+    public function fromString(string $string)
+    {
+        parse_str($string, $data);
+        $this->fromArray($data);
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->toString();
+    }
+
+    public function toString()
+    {
+        return http_build_query($this->toArray());
+    }
+}
---
+++ tests/Core/QueryStringTest.php
@@ -0,0 +1,23 @@
+<?php
+
+class QueryStringTest extends PHPUnit_Framework_TestCase
+{
+    public function testFromString()
+    {
+        $url = new \T4\Core\QueryString();
+        $url->fromString('foo=bar&baz=42');
+        $this->assertEquals('bar', $url->foo);
+        $this->assertEquals(42, $url->baz);
+    }
+
+    public function testToString()
+    {
+        $url = new \T4\Core\QueryString(['foo' => 'bar', 'baz' => 42]);
+        $this->assertEquals('foo=bar&baz=42', $url->toString());
+    }
+
+    public function testConstructorToString()
+    {
+        $this->assertEquals('foo=bar&baz=42', new \T4\Core\QueryString(['foo' => 'bar', 'baz' => 42]));
+    }
+}
