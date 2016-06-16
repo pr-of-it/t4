d8f6cf23a5ef333c0068b54b5b2b9b6dfacb84a8
Coooller <coooller@gmail.com>
2016-06-16 16:31:38 +0300

    Правка класса Sort

--- framework/Html/Sort.php
+++ framework/Html/Sort.php
@@ -6,25 +6,33 @@ use T4\Core\Std;
 use T4\Core\Url;
 
 class Sort
-  extends Std
+    extends Std
 {
+
+    public function __construct($data)
+    {
+        if (!empty($this->by)) {
+            parent::__construct($data);
+        }
+
+    }
 
     public function makeUrl(Url $url = null, $field = 'sort')
     {
-        if (null == $url){
+        if (null == $url) {
             $url = new Url();
         }
-        if (!empty($this->by)){
+        if (!empty($this->by)) {
             $url->query[$field] = $this;
         }
         return $url;
-        
+
     }
 
     public function getOptions($options = [])
     {
-        if (!empty($this->by)){
-            return array_merge($options, ['order' => $this->by]);
+        if (!empty($this->by)) {
+            return array_merge($options, ['order' => $this->by . ' ' . $this->direction]);
         } else {
             return $options;
         }
