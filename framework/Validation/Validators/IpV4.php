4a60f86f4f2bcea56db932b78d0733b73a4a752b
Степанцев Альберт <126@gosbroker.ru>
2016-08-24 18:00:57 +0300

    + validators are added

---
+++ framework/Validation/Validators/IpV4.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace T4\Validation\Validators;
+
+use T4\Core\Validator;
+use T4\Validation\Exceptions\EmptyValue;
+use T4\Validation\Exceptions\InvalidIpV4;
+
+class IpV4 extends Validator
+{
+
+    public function validate($value): bool
+    {
+        if (empty($value)) {
+            throw new EmptyValue($value);
+        }
+
+        if ( false === filter_var($value, \FILTER_VALIDATE_IP, \FILTER_FLAG_IPV4) ) {
+            throw new InvalidIpV4($value);
+        }
+
+        return true;
+    }
+
+}
---
+++ tests/Validation/Validators/IpV4Test.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace T4\Tests\Validation\Validators;
+
+use T4\Validation\Error;
+use T4\Validation\Exceptions\EmptyValue;
+use T4\Validation\Exceptions\InvalidIpV4;
+use T4\Validation\Validators\IpV4;
+
+require_once realpath(__DIR__ . '/../../../framework/boot.php');
+
+class IpV4Test extends \PHPUnit_Framework_TestCase
+{
+
+    public function testPositive()
+    {
+        $validator = new IpV4();
+        $result = $validator('8.8.8.8');
+        $this->assertTrue($result);
+    }
+
+    public function testNegative1()
+    {
+        $value = '';
+        try {
+            $validator = new IpV4();
+            $validator($value);
+        } catch (Error $e) {
+            $this->assertInstanceOf(EmptyValue::class, $e);
+            $this->assertEquals($value, $e->value);
+            return;
+        }
+        $this->assertTrue(false);
+    }
+
+    public function testNegative2()
+    {
+        $value = '8.8.8';
+        try {
+            $validator = new IpV4();
+            $validator($value);
+        } catch (Error $e) {
+            $this->assertInstanceOf(InvalidIpV4::class, $e);
+            $this->assertEquals($value, $e->value);
+            return;
+        }
+        $this->assertTrue(false);
+    }
+
+    public function testNegative3()
+    {
+        $value = '300.200.100.50';
+        try {
+            $validator = new IpV4();
+            $validator($value);
+        } catch (Error $e) {
+            $this->assertInstanceOf(InvalidIpV4::class, $e);
+            $this->assertEquals($value, $e->value);
+            return;
+        }
+        $this->assertTrue(false);
+    }
+
+}
