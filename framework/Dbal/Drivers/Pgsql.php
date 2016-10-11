ac8e8f6da9d65c7513b515b94a18bdfdbeff192e
gvorotilov <248@gosbroker.ru>
2016-10-11 17:46:28 +0300

    fixes doubled join after merging options in Pgsql::findAll() removes redundant step 'params' from last loop in QueryBuilder::merge()

--- framework/Dbal/Drivers/Pgsql.php
+++ framework/Dbal/Drivers/Pgsql.php
@@ -349,7 +349,7 @@ class Pgsql
 
     public function findAll($class, $options = [])
     {
-        $query = new QueryBuilder($options);
+        $query = new QueryBuilder();
         $query
             ->select('t1.*')
             ->from($class::getTableName())
