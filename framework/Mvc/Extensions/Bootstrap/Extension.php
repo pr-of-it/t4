37ab4b07296510ffe19df68f25007f9f9a05fc83
Albert Stepantsev <Albert.Stepantsev@open.ru>
2017-02-14 12:33:28 +0300

    + Bootstrap extension development

--- framework/Mvc/Extensions/Bootstrap/Extension.php
+++ framework/Mvc/Extensions/Bootstrap/Extension.php
@@ -12,29 +12,28 @@ class Extension
         $assets = $this->app->assets;
         $theme = $this->config->theme;
 
+        $version = (int)$this->config->version ?? '3.3.7';
+
         if (isset($this->config->location) && 'local' == $this->config->location) {
 
-            $assets->publish($this->assetsPath . '/lib');
-
-            $assets->publishCssFile($this->assetsPath . '/lib/css/bootstrap.min.css');
-
+            $assets->publish($this->assetsPath . '/lib/' . $version);
+            $assets->publishCssFile($this->assetsPath . '/lib/' . $version . '/css/bootstrap.min.css');
             if (!empty($this->config->theme)) {
-                $assets->publishCssFile($this->assetsPath . '/lib/css/' . $theme . '/bootstrap.min.css');
+                $assets->publishCssFile($this->assetsPath . '/lib/' . $version . '/css/' . $theme . '/bootstrap.min.css');
             } else {
-                $assets->publishCssFile($this->assetsPath . '/lib/css/bootstrap-theme.min.css');
+                $assets->publishCssFile($this->assetsPath . '/lib/' . $version . '/css/bootstrap-theme.min.css');
             }
-
-            $assets->publishJsFile($this->assetsPath . '/lib/js/bootstrap.min.js');
+            $assets->publishJsFile($this->assetsPath . '/lib/' . $version . '/js/bootstrap.min.js');
 
         } else {
 
-            $assets->registerCssUrl('//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css');
+            $assets->registerCssUrl('https://maxcdn.bootstrapcdn.com/bootstrap/' . $version . '/css/bootstrap.min.css');
             if (!empty($this->config->theme)) {
-                $assets->registerCssUrl('//maxcdn.bootstrapcdn.com/bootswatch/3.3.2/' . $theme . '/bootstrap.min.css');
+                $assets->registerCssUrl('https://maxcdn.bootstrapcdn.com/bootswatch/' . $version . '/' . $theme . '/bootstrap.min.css');
             } else {
-                $assets->registerCssUrl('//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css');
+                $assets->registerCssUrl('https://maxcdn.bootstrapcdn.com/bootstrap/' . $version . '/css/bootstrap-theme.min.css');
             }
-            $assets->registerJsUrl('//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js');
+            $assets->registerJsUrl('https://maxcdn.bootstrapcdn.com/bootstrap/' . $version . '/js/bootstrap.min.js');
 
         }
     }
