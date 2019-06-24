a8048c130b6058939b68a5e341371589dce11741
Evgenii Ryshkov <eryshkov@gmail.com>
2019-06-24 11:22:32 +0300

    PgsqlDriverTest.php new tests for null option added

--- tests/Dbal/Drivers/PgsqlDriverTest.php
+++ tests/Dbal/Drivers/PgsqlDriverTest.php
@@ -51,6 +51,14 @@ class PgsqlDriverTest extends \PHPUnit\Framework\TestCase {
             $reflector->invokeArgs($driver, ['table', 'foo', ['type' => 'relation']])
         );
         $this->assertEquals(
+            '"foo" BIGINT NOT NULL',
+            $reflector->invokeArgs($driver, ['table', 'foo', ['type' => 'relation', 'null' => false]])
+        );
+        $this->assertEquals(
+            '"foo" BIGINT',
+            $reflector->invokeArgs($driver, ['table', 'foo', ['type' => 'relation', 'null' => true]])
+        );
+        $this->assertEquals(
             '"foo" SERIAL',
             $reflector->invokeArgs($driver, ['table', 'foo', ['type' => 'serial']])
         );
